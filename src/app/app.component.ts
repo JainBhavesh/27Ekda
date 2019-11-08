8a281244466e058c5322a6c76cfe65becbdfac65
ravi-ashara <raviashar94@gmail.com>
2019-11-08 09:53:52 +0530

    Change code as per Feedback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Router } from '@angular/router';
 import { AlertModule } from './Module/alert/alert.module';
-import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
+// import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
 import { BasicService } from './service/Basic/basic.service';
 import { Storage } from '@ionic/storage';
 
@@ -48,7 +48,7 @@ export class AppComponent {
     public router: Router,
     public alertModule: AlertModule,
     public navCtrl: NavController,
-    private push: Push,
+    // private push: Push,
     public bs: BasicService,
     public storage: Storage
   ) {
@@ -109,49 +109,49 @@ export class AppComponent {
     });
   }
 
-  pushNotifictions() {
-    const options: PushOptions = {
-      android: {
-        senderID: '690486240690'
-      },
-      browser: {},
-      ios: {
-        sound: true,
-        badge: true
-      },
-      windows: {},
+  // pushNotifictions() {
+  //   const options: PushOptions = {
+  //     android: {
+  //       senderID: '690486240690'
+  //     },
+  //     browser: {},
+  //     ios: {
+  //       sound: true,
+  //       badge: true
+  //     },
+  //     windows: {},
 
-    };
+  //   };
 
-    const pushObject: PushObject = this.push.init(options);
+  //   const pushObject: PushObject = this.push.init(options);
 
-    pushObject.on('registration').subscribe((data: any) => {
+  //   pushObject.on('registration').subscribe((data: any) => {
 
-      console.log('Device registered' + data);
-      console.log('Device registrationId ' + data.registrationId);
+  //     console.log('Device registered' + data);
+  //     console.log('Device registrationId ' + data.registrationId);
 
-      this.bs.deviceToken = data.registrationId;
-    });
+  //     this.bs.deviceToken = data.registrationId;
+  //   });
 
-    pushObject.on('notification').subscribe((data: any) => {
-      console.log('Received a notification ' + JSON.stringify(data));
-      var extraData = JSON.parse(JSON.stringify(data.additionalData));
-      if (data.additionalData.foreground) {
-        // if application open
-        console.log('in foreground');
+  //   pushObject.on('notification').subscribe((data: any) => {
+  //     console.log('Received a notification ' + JSON.stringify(data));
+  //     var extraData = JSON.parse(JSON.stringify(data.additionalData));
+  //     if (data.additionalData.foreground) {
+  //       // if application open
+  //       console.log('in foreground');
 
-      } else {
-        // In background
-        // setTimeout(() => {
-        //   this.navCtrl.navigateForward('');
-        // }, 2000);
-      }
-    });
+  //     } else {
+  //       // In background
+  //       // setTimeout(() => {
+  //       //   this.navCtrl.navigateForward('');
+  //       // }, 2000);
+  //     }
+  //   });
 
 
-    pushObject.on('error').subscribe(error => {
-      console.error('Error with Push plugin' + JSON.stringify(error));
-    });
+  //   pushObject.on('error').subscribe(error => {
+  //     console.error('Error with Push plugin' + JSON.stringify(error));
+  //   });
 
-  }
+  // }
 }
